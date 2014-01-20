5ff090cfb427ab1575108a774c89259b3bc2da2b
Eric Amador <eric@eric-nimbis.local>
2014-01-20 01:24:10 -0500

    Fixed incorrect package data in setup.py

--- setup.py
+++ setup.py
@@ -21,11 +21,11 @@ setup(
     author_email='eric.amador14@gmail.com',
     url='https://github.com/amadornimbis/cmsplugin-newsplus/',
     packages=find_packages(),
-    package_data={'cmsplugin_news': [
+    package_data={'cmsplugin_newsplus': [
         'locale/de/LC_MESSAGES/*',
-        'templates/cmsplugin_news/*.html',
-        'templates/cmsplugin_news/feeds/*.html',
-        'templates/cmsplugin_news/widgets/*.html']
+        'templates/cmsplugin_newsplus/*.html',
+        'templates/cmsplugin_newsplus/feeds/*.html',
+        'templates/cmsplugin_newsplus/widgets/*.html']
     },
     license='BSD',
     classifiers=[
