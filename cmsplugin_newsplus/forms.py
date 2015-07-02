4e1f09e2a6418786dccc89c0914420904f34e3d7
Dean Kiourtsis <djkiourtsis@wpi.edu>
2015-07-02 11:26:43 -0400

    Added flake8 to Makefile, requirements, and travis.  Fixes for flake8.

--- cmsplugin_newsplus/forms.py
+++ cmsplugin_newsplus/forms.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 from django import forms
-from django.conf import settings
 
 from cms.plugin_pool import plugin_pool
 from djangocms_text_ckeditor.widgets import TextEditorWidget
