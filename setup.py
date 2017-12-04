c9d337bf8ae47bf880de67b09bca9ccf9380de1a
Brian Schott <brian.schott@nimbisservices.com>
2017-12-04 11:55:33 -0800

    Limit Django to <2.0.
    Django 2.0 is currently failing tests, so limit to < Django 2.0.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        'Django',
+        'Django<2.0',
         'django-cms >= 2.4',
         'djangocms-text-ckeditor >= 2.0',
         'Pillow',
